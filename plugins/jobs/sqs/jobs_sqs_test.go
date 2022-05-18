d1f2a7a0fed5cf7ce41647e5a73ec8074c2cf015
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-05-18 19:20:46 +0200

    update ci
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- plugins/jobs/sqs/jobs_sqs_test.go
+++ plugins/jobs/sqs/jobs_sqs_test.go
@@ -828,8 +828,6 @@ func TestSQSRawPayload(t *testing.T) {
 	wg.Wait()
 
 	assert.Equal(t, 1, oLogger.FilterMessageSnippet("get raw payload").Len())
-	assert.Equal(t, 1, oLogger.FilterMessageSnippet("pipeline was started").Len())
-	assert.Equal(t, 1, oLogger.FilterMessageSnippet("pipeline was stopped").Len())
 	assert.Equal(t, 1, oLogger.FilterMessageSnippet("job processing was started").Len())
 	assert.Equal(t, 1, oLogger.FilterMessageSnippet("job was processed successfully").Len())
 }
