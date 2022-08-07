450af35586909a04fa8d5faaa03317f9c017c4f1
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-08-07 13:34:07 +0200

    Use JsonSerializer in the Consumer
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- php_test_files/jobs/jobs_ok_slow.php
+++ php_test_files/jobs/jobs_ok_slow.php
@@ -8,12 +8,13 @@ use Spiral\Goridge;
 use Spiral\RoadRunner;
 use Spiral\Goridge\StreamRelay;
 use Spiral\RoadRunner\Jobs\Consumer;
+use Spiral\RoadRunner\Jobs\Serializer\JsonSerializer;
 
 ini_set('display_errors', 'stderr');
 require dirname(__DIR__) . "/vendor/autoload.php";
 
 $rr = new RoadRunner\Worker(new StreamRelay(\STDIN, \STDOUT));
-$consumer = new Consumer($rr);
+$consumer = new Consumer($rr, new JsonSerializer);
 
 while ($task = $consumer->waitTask()) {
     try {
