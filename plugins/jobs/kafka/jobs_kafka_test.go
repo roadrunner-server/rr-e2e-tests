bca557fb008d8154eabc3d5c4c3d150eeda3672d
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-08-10 15:02:13 +0200

    add kafka declare CG tests
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- plugins/jobs/kafka/jobs_kafka_test.go
+++ plugins/jobs/kafka/jobs_kafka_test.go
@@ -244,6 +244,107 @@ func TestKafkaInit(t *testing.T) {
 	assert.GreaterOrEqual(t, oLogger.FilterMessageSnippet("job was processed successfully").Len(), 1000)
 }
 
+func TestKafkaDeclareCG(t *testing.T) {
+	cont, err := endure.NewContainer(nil, endure.SetLogLevel(endure.ErrorLevel))
+	assert.NoError(t, err)
+
+	cfg := &config.Plugin{
+		Version: "2.11.0",
+		Path:    "configs/.rr-kafka-declare.yaml",
+		Prefix:  "rr",
+	}
+
+	l, oLogger := mocklogger.ZapTestLogger(zap.DebugLevel)
+	err = cont.RegisterAll(
+		cfg,
+		&server.Plugin{},
+		&rpcPlugin.Plugin{},
+		&jobs.Plugin{},
+		&kp.Plugin{},
+		l,
+		&resetter.Plugin{},
+		&informer.Plugin{},
+	)
+	assert.NoError(t, err)
+
+	err = cont.Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ch, err := cont.Serve()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	stopCh := make(chan struct{}, 1)
+
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case e := <-ch:
+				assert.Fail(t, "error", e.Error.Error())
+				err = cont.Stop()
+				if err != nil {
+					assert.FailNow(t, "error", err.Error())
+				}
+			case <-sig:
+				err = cont.Stop()
+				if err != nil {
+					assert.FailNow(t, "error", err.Error())
+				}
+				return
+			case <-stopCh:
+				// timeout
+				err = cont.Stop()
+				if err != nil {
+					assert.FailNow(t, "error", err.Error())
+				}
+				return
+			}
+		}
+	}()
+
+	time.Sleep(time.Second * 3)
+	t.Run("DeclarePipeline", declarePipeCG("test-33"))
+	time.Sleep(time.Second * 2)
+	t.Run("ResumePipeline", helpers.ResumePipes("test-33"))
+	time.Sleep(time.Second * 2)
+
+	t.Run("PushPipeline", helpers.PushToPipe("test-33", false))
+
+	for i := 0; i < 2; i++ {
+		t.Run("PushPipeline", helpers.PushToPipe("test-33", false))
+	}
+
+	time.Sleep(time.Second * 5)
+	t.Run("PausePipeline", helpers.PausePipelines("test-33"))
+
+	for i := 0; i < 2; i++ {
+		t.Run("PushPipeline", helpers.PushToPipe("test-33", false))
+	}
+
+	time.Sleep(time.Second * 2)
+	t.Run("ResumePipeline", helpers.ResumePipes("test-33"))
+	time.Sleep(time.Second * 5)
+
+	t.Run("DestroyPipeline", helpers.DestroyPipelines("test-33"))
+	stopCh <- struct{}{}
+	wg.Wait()
+
+	assert.Equal(t, 5, oLogger.FilterMessageSnippet("job was pushed successfully").Len())
+	assert.Equal(t, 5, oLogger.FilterMessageSnippet("job processing was started").Len())
+	assert.Equal(t, 2, oLogger.FilterMessageSnippet("pipeline was resumed").Len())
+	assert.Equal(t, 1, oLogger.FilterMessageSnippet("pipeline was paused").Len())
+}
+
 func TestKafkaDeclare(t *testing.T) {
 	cont, err := endure.NewContainer(nil, endure.SetLogLevel(endure.ErrorLevel))
 	assert.NoError(t, err)
@@ -313,29 +414,29 @@ func TestKafkaDeclare(t *testing.T) {
 	}()
 
 	time.Sleep(time.Second * 3)
-	t.Run("DeclarePipeline", declarePipe("test-2"))
+	t.Run("DeclarePipeline", declarePipe("test-22"))
 	time.Sleep(time.Second * 2)
-	t.Run("ResumePipeline", helpers.ResumePipes("test-2"))
+	t.Run("ResumePipeline", helpers.ResumePipes("test-22"))
 	time.Sleep(time.Second * 2)
 
-	t.Run("PushPipeline", helpers.PushToPipe("test-2", false))
+	t.Run("PushPipeline", helpers.PushToPipe("test-22", false))
 
 	for i := 0; i < 2; i++ {
-		t.Run("PushPipeline", helpers.PushToPipe("test-2", false))
+		t.Run("PushPipeline", helpers.PushToPipe("test-22", false))
 	}
 
 	time.Sleep(time.Second * 5)
-	t.Run("PausePipeline", helpers.PausePipelines("test-2"))
+	t.Run("PausePipeline", helpers.PausePipelines("test-22"))
 
 	for i := 0; i < 2; i++ {
-		t.Run("PushPipeline", helpers.PushToPipe("test-2", false))
+		t.Run("PushPipeline", helpers.PushToPipe("test-22", false))
 	}
 
 	time.Sleep(time.Second * 2)
-	t.Run("ResumePipeline", helpers.ResumePipes("test-2"))
+	t.Run("ResumePipeline", helpers.ResumePipes("test-22"))
 	time.Sleep(time.Second * 5)
 
-	t.Run("DestroyPipeline", helpers.DestroyPipelines("test-2"))
+	t.Run("DestroyPipeline", helpers.DestroyPipelines("test-22"))
 	stopCh <- struct{}{}
 	wg.Wait()
 
@@ -437,6 +538,63 @@ func TestKafkaJobsError(t *testing.T) {
 }
 
 func declarePipe(topic string) func(t *testing.T) {
+	return func(t *testing.T) {
+		conn, err := net.Dial("tcp", "127.0.0.1:6001")
+		assert.NoError(t, err)
+		client := rpc.NewClientWithCodec(goridgeRpc.NewClientCodec(conn))
+
+		pipe := &jobsProto.DeclareRequest{
+			Pipeline: map[string]string{
+				"driver":   "kafka",
+				"name":     topic,
+				"priority": "3",
+				"topic":    topic,
+				"partitions_offsets": `
+				{
+				     "0": "0",
+				     "1": "0",
+				     "2": "0"
+				}`,
+				"max_open_requests": "100",
+				"client_id":         "roadrunner",
+				"version":           "3.2.0.0",
+
+				// create_topics
+				"replication_factor": "1",
+				// "replica_assignment": `
+				// {
+				//      "0": [1,2,3],
+				//      "1": [1,2,3],
+				//      "2": [1,2,3]
+				// }
+				// `,
+				"config_entries": `
+    			{
+                      "max.message.bytes": "100000"
+				}
+				`,
+
+				// producer
+				"max_message_bytes": "10000",
+				"required_acks":     "-1",
+				"timeout":           "10",
+				"compression_codec": "snappy",
+				"compression_level": "100",
+				"idempotent":        "false",
+
+				// consumer
+				// "heartbeat_interval": "3",
+				// "session_timeout":    "10",
+			},
+		}
+
+		er := &jobsProto.Empty{}
+		err = client.Call("jobs.Declare", pipe, er)
+		assert.NoError(t, err)
+	}
+}
+
+func declarePipeCG(topic string) func(t *testing.T) {
 	return func(t *testing.T) {
 		conn, err := net.Dial("tcp", "127.0.0.1:6001")
 		assert.NoError(t, err)
