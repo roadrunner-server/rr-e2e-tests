082d64f11425867d268811727715419a001eaa84
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-08-10 19:53:45 +0200

    add more kafka tests
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- plugins/jobs/durability/jobs_with_toxics_test.go
+++ plugins/jobs/durability/jobs_with_toxics_test.go
@@ -557,3 +557,132 @@ func TestDurabilityKafka(t *testing.T) {
 		time.Sleep(time.Second * 15)
 	})
 }
+
+func TestDurabilityKafkaCG(t *testing.T) {
+	cmd := exec.Command("docker-compose", "-f", "../../../env/docker-compose-kafka.yaml", "up")
+	err := cmd.Start()
+	require.NoError(t, err)
+
+	go func() {
+		_ = cmd.Wait()
+	}()
+
+	time.Sleep(time.Second * 40)
+
+	cont, err := endure.NewContainer(nil, endure.SetLogLevel(endure.ErrorLevel), endure.GracefulShutdownTimeout(time.Second*30))
+	require.NoError(t, err)
+
+	cfg := &config.Plugin{
+		Version: "2.11.0",
+		Path:    "configs/.rr-kafka-durability-redial-cg.yaml",
+		Prefix:  "rr",
+	}
+
+	l, oLogger := mocklogger.ZapTestLogger(zap.DebugLevel)
+	err = cont.RegisterAll(
+		cfg,
+		&server.Plugin{},
+		&rpcPlugin.Plugin{},
+		l,
+		&jobs.Plugin{},
+		&resetter.Plugin{},
+		&informer.Plugin{},
+		&kafka.Plugin{},
+	)
+	require.NoError(t, err)
+
+	err = cont.Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ch, err := cont.Serve()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	stopCh := make(chan struct{}, 1)
+
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case e := <-ch:
+				assert.Fail(t, "error", e.Error.Error())
+				err = cont.Stop()
+				if err != nil {
+					assert.FailNow(t, "error", err.Error())
+				}
+			case <-sig:
+				err = cont.Stop()
+				if err != nil {
+					assert.FailNow(t, "error", err.Error())
+				}
+				return
+			case <-stopCh:
+				// timeout
+				err = cont.Stop()
+				if err != nil {
+					assert.FailNow(t, "error", err.Error())
+				}
+				return
+			}
+		}
+	}()
+
+	time.Sleep(time.Second * 5)
+	cmd2 := exec.Command("docker-compose", "-f", "../../../env/docker-compose-kafka.yaml", "down")
+	err = cmd2.Start()
+	require.NoError(t, err)
+
+	go func() {
+		_ = cmd2.Wait()
+	}()
+
+	time.Sleep(time.Second * 10)
+
+	t.Run("PushPipelineWhileRedialing-1", helpers.PushToPipeErr("test-1"))
+	t.Run("PushPipelineWhileRedialing-2", helpers.PushToPipeErr("test-2"))
+
+	cmd3 := exec.Command("docker-compose", "-f", "../../../env/docker-compose-kafka.yaml", "up")
+	err = cmd3.Start()
+	require.NoError(t, err)
+
+	go func() {
+		_ = cmd3.Wait()
+	}()
+
+	time.Sleep(time.Second * 25)
+
+	t.Run("PushPipelineWhileRedialing-1", helpers.PushToPipe("test-1", false))
+	t.Run("PushPipelineWhileRedialing-2", helpers.PushToPipe("test-2", false))
+	time.Sleep(time.Second * 5)
+	t.Run("DestroyPipelines", helpers.DestroyPipelines("test-1", "test-2"))
+	time.Sleep(time.Second * 20)
+
+	stopCh <- struct{}{}
+	wg.Wait()
+
+	assert.Equal(t, 2, oLogger.FilterMessageSnippet("pipeline was started").Len())
+	assert.Equal(t, 2, oLogger.FilterMessageSnippet("pipeline was stopped").Len())
+	assert.Equal(t, 2, oLogger.FilterMessageSnippet("job processing was started").Len())
+	assert.Equal(t, 2, oLogger.FilterMessageSnippet("job push error").Len())
+
+	t.Cleanup(func() {
+		cmd4 := exec.Command("docker-compose", "-f", "../../../env/docker-compose-kafka.yaml", "down")
+		err = cmd4.Start()
+		require.NoError(t, err)
+
+		go func() {
+			_ = cmd4.Wait()
+		}()
+
+		time.Sleep(time.Second * 15)
+	})
+}
